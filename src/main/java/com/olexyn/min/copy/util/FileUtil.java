0b4980e7c836c59363615507edfd1e9df9cc11b1
io42630 <ivan@olexyn.com>
2025-06-22 19:45:15 +0200

    add lastModified()

--- src/main/java/com/olexyn/min/copy/util/FileUtil.java
+++ src/main/java/com/olexyn/min/copy/util/FileUtil.java
@@ -2,13 +2,14 @@ package com.olexyn.min.copy.util;
 
 import com.olexyn.min.log.LogU;
 import lombok.experimental.UtilityClass;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Instant;
 
 @UtilityClass
 public class FileUtil {
-
 
 
     public static long size(Path path) {
@@ -17,6 +18,17 @@ public class FileUtil {
         } catch (Exception e) {
             LogU.warnPlain(e.getMessage());
             return 0;
+        }
+    }
+
+
+
+    public static @Nullable Instant lastModified(Path path) {
+        try {
+            return Files.getLastModifiedTime(path).toInstant();
+        } catch (Exception e) {
+            LogU.warnPlain(e.getMessage());
+            return null;
         }
     }
 }
